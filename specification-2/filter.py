83708253e6eda024c005d9313fb2c7ee27821e2b
b9014614 <t.o'shaughnessy1@newcastle.ac.uk>
2019-11-04 10:08:26 +0000

    added all filters and cleaned up main

--- specification-2/filter.py
+++ specification-2/filter.py
@@ -8,6 +8,13 @@ def filter(img):
         b:Blur
         c:Contour
         d:Detail
+        e:Edge Enhance
+        e+:Edge Enhance More
+        em:Emboss
+        f:Find Edges
+        s:Sharpen
+        sm:Smooth
+        sm+:Smooth More
     """)
     if filter == "b":
         image = image.filter(ImageFilter.BLUR)
@@ -15,6 +22,20 @@ def filter(img):
         image = image.filter(ImageFilter.CONTOUR)
     elif filter == "d":
         image = image.filter(ImageFilter.DETAIL)
+    elif filter == "e":
+        image = image.filter(ImageFilter.EDGE_ENHANCE)
+    elif filter == "e+":
+        image = image.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    elif filter == "em":
+        image = image.filter(ImageFilter.EMBOSS)
+    elif filter == "f":
+        image = image.filter(ImageFilter.FIND_EDGES)
+    elif filter == "s":
+        image = image.filter(ImageFilter.SHARPEN)
+    elif filter == "sm":
+        image = image.filter(ImageFilter.SMOOTH)
+    elif filter == "sm+":
+        image = image.filter(ImageFilter.SMOOTH_MORE)
     new_name = "." + img.split(".")[1] + filter + ".jpg"
     print(new_name)
     image.save(new_name, "JPEG")
--- specification-2/main.py
+++ specification-2/main.py
@@ -1,8 +1,19 @@
 from os import listdir
-from thumbnail import thumbnail
 from filter import filter
+from thumbnail import thumbnail
 if __name__ ==  "__main__":
     images = []
     for img in listdir("./img"):
         images.append("./img/"+img)
-    filter(images[1])
+    choice = input("""
+        t:Thumbnail
+        f:Filter
+    """)
+    if choice == "t":
+        func = thumbnail
+    elif choice == "f":
+        func = filter
+    for x in range(len(listdir("./img"))):
+        print(str(x)+":"+images[x])
+    img_choice = int(input("Which image index?"))
+    func(images[img_choice])
