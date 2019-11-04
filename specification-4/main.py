c4c1044580b622b03246a8b8b2d627808073c68c
b9014614 <t.o'shaughnessy1@newcastle.ac.uk>
2019-11-04 11:29:43 +0000

    gui skeleton done

--- specification-4/main.py
+++ specification-4/main.py
@@ -1,19 +1,25 @@
 from tkinter import Tk,Button,Entry,Label,Frame,Text,END
 def add(name,values,text):
+    if "" in [name.get(),values.get()]:
+        print("Please enter a name and values")
+        return
     text.insert(END,name.get()+":{"+values.get()+"}\n")
     name.delete(0,"end")
     values.delete(0,"end")
+def calc():
+    print("dummy")
 if __name__ == "__main__":
     window = Tk()
 
 
-
     listFrame = Frame(window)
     listText = Text(listFrame, width=20,height=10)
-    listText.pack()
+    listLabel = Label(listFrame,text = "Sets")
+    listLabel.grid(row=0,column=0)
+    listText.grid(row=1,column=0)
 
 
-    addFrame = Frame(window)
+    addFrame = Frame(window,borderwidth=10,highlightcolor="black",highlightbackground="black",highlightthickness=1)
     addTextName = Entry(addFrame, width=5)
     addTextNameLabel = Label(addFrame, text="Set Name")
     addTextSet = Entry(addFrame, width=30)
@@ -25,7 +31,23 @@ if __name__ == "__main__":
     addTextSet.grid(row=1, column=1)
     addButton.grid(row=2, column=0)
 
+    cartFrame = Frame(window)
+    cartList = Text(cartFrame, width=30,height=20)
+    cartLabel = Label(cartFrame,text="Cartesian Product")
+    cartLabel.grid(row=0,column=0)
+    cartList.grid(row=1,column=0)
+
+    calcCart = Frame(window)
+    calcLabel = Label(calcCart,text="Cartesian Sets")
+    calcText = Entry(calcCart,width = 10)
+    calcButton = Button(calcCart,command=calc,text="Calculate Cartesian Product")
+    calcLabel.grid(row=0,column=0)
+    calcText.grid(row=0,column=1)
+    calcButton.grid(row=1,column=0)
+
     listFrame.grid(row=0, column=0)
     addFrame.grid(row=0, column=1)
+    cartFrame.grid(row=0,column=2)
+    calcCart.grid(row=1,column=1)
 
     window.mainloop()
