c6f322f98e3740562be1d7529d6749af81563928
Tom O'Shaughnessy <b9014614@ncl.ac.uk>
2019-11-07 19:19:12 +0000

    also fixed save location for thumbnails, not just filters

--- specification-2/thumbnail.py
+++ specification-2/thumbnail.py
@@ -3,6 +3,6 @@ def thumbnail(img):
     print(img)
     image = Image.open(img)
     image = image.resize((100,100), Image.ANTIALIAS)
-    new_name = "." + img.split(".")[1] + "thumb.jpg"
+    new_name = ".." + img[2:].split(".")[0] + filter + ".jpg"
     image.save(new_name, "JPEG")
     image.show()
