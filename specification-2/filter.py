46efe664410adb54dbc9b4813fc4d8c619a8caf1
b9031331 <b9031331@newcastle.ac.uk>
2019-11-11 10:10:53 +0000

    "Modify the RGB values of images to produce a new filter" added greyscale filter and primary filter - Zayn

--- specification-2/filter.py
+++ specification-2/filter.py
@@ -38,51 +38,94 @@ def filter(img):
     elif filter == "sm+":
         image = image.filter(ImageFilter.SMOOTH_MORE)
     elif filter == "g":
-        # Create a new image with the given size
-        def create_image(i, j):
-            image = Image.new("RGB", (i, j), "white")
-            return image
-
-        # Get the pixel from the given image
-        def get_pixel(image, i, j):
-            # Inside image bounds?
-            width, height = image.size
-            if i > width or j > height:
-                return None
-
-            # Get Pixel
-            pixel = image.getpixel((i, j))
-            return pixel
-
-        # Create a Grayscale version of the image
-        def convert_grayscale(image):
-            # Get size
-            width, height = image.size
-
-            # Create new Image and a Pixel Map
-            new = create_image(width, height)
-            pixels = new.load()
-
-            # Transform to grayscale
-            for i in range(width):
-                for j in range(height):
-                    # Get Pixel
-                    pixel = get_pixel(image, i, j)
-
-                    # Get R, G, B values (This are int from 0 to 255)
-                    red = pixel[0]
-                    green = pixel[1]
-                    blue = pixel[2]
-
-                    # Transform to grayscale
-                    gray = (red * 0.299) + (green * 0.587) + (blue * 0.114)
-
-                    # Set Pixel in new image
-                    pixels[i, j] = (int(gray), int(gray), int(gray))
-
-            # Return new image
-            return new
+        img = Image.open('THEIMAGEGOESHERE').convert('LA')
+        img.save('greyscale.png')
+    elif filter == "primary":
+#        new = convert_primary(original)
+#           save_image(new, 'NEWIMAGE')
     new_name = "." + img.split(".")[1] + filter + ".jpg"
     print(new_name)
     image.save(new_name, "JPEG")
     image.show()
+
+# # Imported PIL Library from PIL import Image
+#
+# # Open an Image
+# def open_image(path):
+#   newImage = Image.open(path)
+#   return newImage
+#
+# # Save Image
+# def save_image(image, path):
+#   image.save(path, 'png')
+#
+#
+# Create a new image with the given size
+def create_image(i, j):
+  image = Image.new("RGB", (i, j), "white")
+  return image
+
+
+# Get the pixel from the given image
+def get_pixel(image, i, j):
+  # Inside image bounds?
+  width, height = image.size
+  if i > width or j > height:
+    return None
+
+  # Get Pixel
+  pixel = image.getpixel((i, j))
+  return pixel
+
+# Create a Primary Colors version of the image
+def convert_primary(image):
+  # Get size
+  width, height = image.size
+
+  # Create new Image and a Pixel Map
+  new = create_image(width, height)
+  pixels = new.load()
+
+  # Transform to primary
+  for i in range(width):
+    for j in range(height):
+      # Get Pixel
+      pixel = get_pixel(image, i, j)
+
+      # Get R, G, B values (This are int from 0 to 255)
+      red =   pixel[0]
+      green = pixel[1]
+      blue =  pixel[2]
+
+      # Transform to primary
+      if red > 127:
+        red = 255
+      else:
+        red = 0
+      if green > 127:
+        green = 255
+      else:
+        green = 0
+      if blue > 127:
+        blue = 255
+      else:
+        blue = 0
+
+      # Set Pixel in new image
+      pixels[i, j] = (int(red), int(green), int(blue))
+
+  # Return new image
+  return new
+#
+#
+# # Main
+# if __name__ == "__main__":
+#   # Load Image (JPEG/JPG needs libjpeg to load)
+#   original = open_image('THEIMAGE')
+#
+#   # Example Pixel Color
+#   print('Color: ' + str(get_pixel(original, 0, 0)))
+#
+#   # Convert to Primary and save
+#   new = convert_primary(original)
+#   save_image(new, 'Prinny_primary.png')
