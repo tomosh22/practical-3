b005d0f433f71b0b207078e5e4e7e34a9cb7032d
b9021147 <b9021147@ncl.ac.uk>
2019-11-13 20:59:11 +0000

    Spec 1 - V4 - 13/11/2019 - 20:52 + Added feature: user can create custom bar chart + Added feature: user specifies how many of the most used words are outputted to the csv file + Added another try-catch (user-friendly feature) + Added progress messages (user-friendly feature)

--- specification-1/Development_History/BUILDING_2D-BubbleSort.py
+++ specification-1/Development_History/BUILDING_2D-BubbleSort.py
@@ -1,7 +1,33 @@
 # Create a bubble sort, resulting in the list being modified to ascending order <smallest-Largest>
 theInfoArray = [["the",10],["fish",15],["bubble",8],["stumped",11],["unary",4],["binary",2],["ternary",6]]
-temp = [["",0]]
 
+        # OBJECTIVE 3 - Writing top 10 most used words to a CSV file
+        # The 2D array storing words and quantity is bubble sorted, but only the top ten elements for efficiency
+quantityOfMostFrequent = int(input("Enter the number of most used words to be written to a CSV file: "))
+theBubbleArray = theInfoArray
+topTen = []
+tempWord = theBubbleArray[0][0]
+tempCount = theBubbleArray[0][1]
+index = 0
+for loop in range(0,quantityOfMostFrequent):
+        for i in range(0,len(theBubbleArray)):
+            print(theBubbleArray[i][1], tempCount)
+            if theBubbleArray[i][1] > tempCount:
+                tempWord = theBubbleArray[i][0]
+                tempCount = theBubbleArray[i][1]
+                index = i
+        theBubbleArray[index][1] = 0
+        if tempCount != 0:
+            topTen.append([tempWord,tempCount])
+        else:
+            print("Upper limit has been reached")# Call next function (the limit has been reached)
+        tempCount = 0
+
+print(topTen)
+
+
+
+'''
 loop = True
 swapped = False
 while loop == True:
@@ -21,6 +47,6 @@ while loop == True:
         loop = False
 
 print(theInfoArray)
-
+'''
 
 
